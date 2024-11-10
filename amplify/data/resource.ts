3620c7019a7993770de1a6c7a32b69031a430ceb
Justin Getzke <justn.getzke@web.de>
2024-11-10 13:54:51 +0100

    init amplify

---
+++ amplify/data/resource.ts
@@ -0,0 +1,25 @@
+import {type ClientSchema, a, defineData} from '@aws-amplify/backend';
+
+const schema = a.schema({
+    Listing: a.model({
+            name: a.string().required(),
+            description: a.string(),
+            address: a.string().required(),
+            solarScore: a.integer(),
+            price: a.integer(),
+        }
+    ).authorization(allow => [
+        allow.owner().to(["create", "update", "delete"]),
+        allow.authenticated().to(["read"]),
+        allow.guest().to(["read"]),
+    ]),
+});
+
+export type Schema = ClientSchema<typeof schema>;
+
+export const data = defineData({
+    schema,
+    authorizationModes: {
+        defaultAuthorizationMode: "userPool",
+    },
+});
